48d2c93b884e303f02f6ffdec8f9cc81cbb30d98
oliviawu <oliviawu0301@gmail.com>
2024-11-18 10:59:39 +0800

    manage

--- manage.php
+++ manage.php
@@ -16,10 +16,57 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>檔案管理功能</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        .item {
+            width: 200px;
+        }
+
+        .item img{
+            width: 100%;
+        }
+
+    </style>
+
 </head>
 <body>
 <h1 class="header">檔案管理練習</h1>
 <!----建立上傳檔案表單及相關的檔案資訊存入資料表機制----->
+<?php
+include_once "function.php";
+echo $_POST['name'];
+echo "<br>";
+dd($_FILES);
+if($_FILES['img']['error']==0){
+    move_uploaded_file($_FILES['img']['tmp_name'],"./files/".$_FILES['img']['name']);
+}else{
+    echo "上傳失敗，請檢查檔案格式或大小是否符合規定";
+}
+
+?>
+
+
+<!----透過資料表來顯示檔案的資訊，並可對檔案執行更新或刪除的工作----->
+<?php
+
+$dirpath="./files";
+
+// $dir=opendir($dirpath)
+$items=scandir($dirpath);
+$items=array_diff($items,array('.','..'));
+
+// 1. foreach
+// 2.div每個東西會佔滿畫面一整行
+// 3.img 會佔滿一個畫面
+foreach($items as $file){
+    echo "<div class='item'>";
+    echo "<img src='{$dirpath}/{$file}'>";
+    echo "</div>";
+}
+
+
+
+
+?>
 
 
 
--- upload.php
+++ upload.php
@@ -9,23 +9,44 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>檔案上傳</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        form {
+            width: 300px;
+            margin: 20px auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+
+        }
+    </style>
 </head>
+
 <body>
- <h1 class="header">檔案上傳練習</h1>
- <!----建立你的表單及設定編碼----->
+    <h1 class="header">檔案上傳練習</h1>
+    <!----建立你的表單及設定編碼----->
+    <!-- enctype => 檔案上傳 -->
+     <!-- multipart =>上傳二進位 -->
+     <form action="manage.php" method="post" enctype="multipart/form-data">
+     <input type="file" name="img" id="file">
+     <input type="text" name="name" id="name">
+     <input type="submit" value="上傳">
+
+
+
+    </form>
 
 
 
 
-
-<!----建立一個連結來查看上傳後的圖檔---->  
+    <!----建立一個連結來查看上傳後的圖檔---->
 
 
 </body>
+
 </html>
